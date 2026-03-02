18b3595b5a16d3e57b32914bfa155e3c5f5ec37e
Diego Hashimoto <diegohashimoto86@gmail.com>
2026-03-02 11:35:06 -0300

    Cadastro e listagem de residentes funcional - Criada função cadastrar_idoso(lista_idosos) - Criada função listar_idosos(lista_idosos) - Integração com menu principal - Lista de dicionários para armazenar múltiplos atributos - Validação de lista vazia na listagem

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def menu_principal():
     print ("4- Sair")
     return input("Escolha a opção numérica: ").strip()
 
-def main(): 
+def main():
+    lista_idosos = [] 
     while True:
         opcao = menu_principal()
         if opcao == "1":
-            print ("Cadastrar")
+            cadastrar_idoso(lista_idosos)
         elif opcao == "2":
-            print("Listar")    
+            listar_idosos(lista_idosos)   
         elif opcao == "3":
             print("Buscar")
         elif opcao == "4":
@@ -23,3 +24,27 @@ def main():
 
 if __name__ == "__main__":
     main()        
+
+
+
+def cadastrar_idoso (lista_idosos):
+    nome = input(" Digite  nome do paciente: ").strip()
+    idade = int(input("Digite a idade do paciente: "))
+    quarto = int(input("Digite o quarto do paciente: "))
+
+    idoso ={
+        "nome": nome,
+        "idade": idade,
+        "quarto": quarto
+
+    }
+    lista_idosos.append(idoso)
+    
+
+def listar_idosos(lista_idosos):
+    if not lista_idosos:
+        print("A lista esta vazia....")
+        return
+    for idoso in lista_idosos:
+        print("-" * 30)
+        print (f"Paciente:{idoso['nome']} \n idade:{idoso['idade']} \n Quarto:{ idoso['quarto']}")
